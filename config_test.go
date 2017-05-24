59b544e93b76ff0c5cb5e5ff395987c603155e64
Ollivier Robert <roberto@keltia.net>
2017-05-24 11:19:48 +0200

    More tests for loadConfig().

--- config_test.go
+++ config_test.go
@@ -9,16 +9,56 @@ import (
 func TestLoadConfigNone(t *testing.T) {
 	baseDir = "test"
     configName = "/nonexistant"
+    empty := &Config{}
 
 	c, err := loadConfig()
-    assert.Empty(t, c, "empty")
+    assert.Equal(t, empty, c, "empty")
 	assert.Error(t, err, "should be in error")
+}
+
+func TestLoadConfigBad(t *testing.T) {
+    baseDir = "test"
+    configName = "bad.toml"
+
+    c, err := loadConfig()
+    assert.Nil(t, c, "nil value")
+    assert.Error(t, err, "should be in error")
 }
 
 func TestLoadConfigGood(t *testing.T) {
     baseDir = "test"
+    configName = "config.toml"
 
     c, err := loadConfig()
     assert.NotEmpty(t, c, "not empty")
     assert.NoError(t, err, "should be fine")
+
+    // Check values
+    cnf := &Config{
+        From:    "foo@example.com",
+        To:      "security@example.com",
+        Subject: "CIMBL",
+        Server:  "SMTP",
+    }
+    assert.Equal(t, cnf, c, "should be equal")
+}
+
+func TestLoadConfigGoodVerbose(t *testing.T) {
+    baseDir = "test"
+    configName = "config.toml"
+    fVerbose = true
+
+    c, err := loadConfig()
+    assert.NotEmpty(t, c, "not empty")
+    assert.NoError(t, err, "should be fine")
+
+    // Check values
+    cnf := &Config{
+        From:    "foo@example.com",
+        To:      "security@example.com",
+        Subject: "CIMBL",
+        Server:  "SMTP",
+    }
+    assert.Equal(t, cnf, c, "should be equal")
+    fVerbose = false
 }
