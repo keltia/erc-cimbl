66ee429f9e40a1d876790baf10cacbeaf54e46a0
Ollivier Robert <roberto@keltia.net>
2017-05-24 10:06:12 +0200

    Add tests for url.go.

--- url_test.go
+++ url_test.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestSetupProxyNoFile(t *testing.T) {
+    file := ""
+    user = ""
+    password = ""
+
+    err := setupProxy(file)
+    assert.Error(t, err, "error")
+    assert.EqualValues(t, "", user, "null user")
+    assert.EqualValues(t, "", password, "null password")
+}
+
+func TestSetupProxyZero(t *testing.T) {
+    file := "test/zero-dbrc"
+    user = ""
+    password = ""
+
+    err := setupProxy(file)
+    assert.Error(t, err, "error")
+    assert.EqualValues(t, "", user, "test user")
+    assert.EqualValues(t, "", password, "test password")
+}
+
+func TestSetupProxyGood(t *testing.T) {
+    file := "test/test-dbrc"
+    user = ""
+    password = ""
+
+    err := setupProxy(file)
+    assert.NoError(t, err, "no error")
+    assert.EqualValues(t, "test", user, "test user")
+    assert.EqualValues(t, "test", password, "test password")
+}
