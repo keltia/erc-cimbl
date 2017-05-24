d57d122a7f55e01f02b0c5ca48c7b662a83f836a
Ollivier Robert <roberto@keltia.net>
2017-05-24 10:06:48 +0200

    Add testing for URLs.

--- url.go
+++ url.go
@@ -1,12 +1,149 @@
 package main
 
-var (
-    URLs []string
-    cntURLs int
+import (
+    "net/http"
+    "net/url"
+    "fmt"
+    "crypto/tls"
+    "encoding/base64"
+    "os"
+    "path/filepath"
+    "log"
+    "strings"
+    "bufio"
 )
 
-func handleURL(url string) {
+var (
+    dbrcFile = filepath.Join(os.Getenv("HOME"), ".dbrc")
 
+    user string
+    password string
+)
+
+func init() {
+    err := setupProxy(dbrcFile)
+    if err != nil {
+        log.Printf("No dbrc file: %v", err)
+    }
+    if fVerbose {
+        log.Printf("Proxy user %s found.", user)
+    }
+}
+
+func setupProxy(file string) (err error) {
+    fh, err := os.Open(file)
+    if err != nil {
+        return fmt.Errorf("Error: can not find %s: %v", dbrcFile, err)
+    }
+    defer fh.Close()
+
+    /*
+    Format:
+    <db>     <user>    <pass>   <type>
+    */
+    scanner := bufio.NewScanner(fh)
+    for scanner.Scan() {
+        line := scanner.Text()
+        if line == "" {
+            break
+        }
+
+        // Replace all tabs by a single space
+        l := strings.Replace(line, "\t", " ", -1)
+        flds := strings.Split(l, " ")
+
+        // Check what we need
+        if flds[0] == "cimbl" {
+            user = flds[1]
+            password = flds[2]
+            break
+        }
+    }
+    if err := scanner.Err(); err != nil {
+        return fmt.Errorf("reading dbrc %s", dbrcFile)
+    }
+
+    if user == "" {
+        return fmt.Errorf("no user/password for cimbl in %s", dbrcFile)
+    }
+
+    return
+}
+
+func setupCheck(str string) (*http.Request, *http.Transport) {
+    /*
+    Proxy code taken from https://github.com/LeoCBS/poc-proxy-https/blob/master/main.go
+     */
+
+    myurl, err := url.Parse(str)
+    if err != nil {
+        return nil, nil
+    }
+
+    req, _ := http.NewRequest("HEAD", str, nil)
+    req.Header.Set("Host", myurl.Host)
+    req.Header.Add("User-Agent", fmt.Sprintf("%s/%s", MyName, MyVersion))
+
+    proxyURL, err = http.ProxyFromEnvironment(req)
+    if err != nil {
+        log.Printf("Error: no user/password for cimbl.")
+        os.Exit(2)
+    } else if proxyURL == nil {
+        log.Println("No proxy configured")
+    } else {
+        auth := fmt.Sprintf("%s:%s", user, password)
+        basic := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+        req.Header.Add("Proxy-Authorization", basic)
+    }
+
+    transport := &http.Transport{
+        Proxy:           http.ProxyURL(proxyURL),
+        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+    }
+    transport.ProxyConnectHeader = req.Header
+
+    return req, transport
+}
+
+func doCheck(req *http.Request, transport *http.Transport) string {
+    client := &http.Client{Transport: transport}
+    req.RequestURI = ""
+
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Printf("erro: %s", err)
+        return ""
+    }
+
+    switch resp.StatusCode {
+    case 302:
+        return fmt.Sprintf("REDIRECT: %s", resp.Header.Get("Location"))
+    case 403:
+        return "BLOCKED-EEC"
+    case 407:
+        return "AUTH"
+    default:
+        return "**BLOCK**"
+    }
+
+}
+
+
+func handleURL(str string) {
+    /*
+    Setup connection including proxy stuff
+     */
+    req, transport := setupCheck(str)
+
+    /*
+    Do the thing, manage redirects, auth requests and stuff
+     */
+    result := doCheck(req, transport)
+    URLs[str] = result
+    cntURLs++
+    if fVerbose {
+        log.Printf("Checking %s: %s", str, result)
+    }
 }
 
 
