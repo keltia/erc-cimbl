38e4581bb502b2fa5c4acb3a1098257b00b845ca
Ollivier Robert <roberto@keltia.net>
2017-05-30 00:18:40 +0200

    Begin testing for doCheck().

--- url_test.go
+++ url_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
+	"net/http"
 )
 
 func TestCheckSetup(t *testing.T) {
@@ -13,6 +15,28 @@ func TestCheckSetup(t *testing.T) {
 }
 
 func TestDoCheck(t *testing.T) {
+	// Check values
+	cnf := &Config{
+		From:    "foo@example.com",
+		To:      "security@example.com",
+		Cc:      "root@example.com",
+		Subject: "CRQ: New URLs/files to be BLOCKED",
+		Server:  "SMTP",
+	}
 
+	ctx := &Context{
+		config: cnf,
+		Paths:  map[string]bool{},
+		URLs:   map[string]string{},
+	}
+
+	str := "http://pontonerywariva342.top/search.php"
+	req, transport := setupCheck(str)
+	assert.NotNil(t, req, "not nil")
+	assert.NotNil(t, transport, "not nil")
+
+	ctx.Client = &http.Client{Transport: transport, Timeout: 10 * time.Second}
+
+	res := doCheck(ctx, req)
+	assert.Equal(t, "**BLOCK**", res, "should be block")
 }
-
