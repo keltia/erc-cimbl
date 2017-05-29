cde1ceb136b9f58964868efee5dc30e03b99e481
Ollivier Robert <roberto@keltia.net>
2017-05-29 15:57:33 +0200

    More tests for mail.go.

--- mail_test.go
+++ mail_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
     "testing"
+    "github.com/stretchr/testify/assert"
+    "fmt"
 )
 
 func TestCreateMail(t *testing.T) {
@@ -9,11 +11,29 @@ func TestCreateMail(t *testing.T) {
 }
 
 func TestAddPaths(t *testing.T) {
+    ctx := &Context{Paths: map[string]bool{"foo.docx": true, }}
+
+
+    res := fmt.Sprintf("%s  %s\n", PathsTmpl, "foo.docx")
+    str := addPaths(ctx)
+    assert.Equal(t, res, str, "should be equal")
+}
+
+func TestAddURLsBlock(t *testing.T) {
+    ctx := &Context{URLs: map[string]string{"http://example.com/malware": "**BLOCK**", }}
+
+    res := fmt.Sprintf("%s  %s\n", URLsTmpl, "http://example.com/malware")
+    str := addURLs(ctx)
+    assert.Equal(t, res, str, "should be equal")
+
 
 }
 
-func TestAddURLs(t *testing.T) {
+func TestAddURLsUnknown(t *testing.T) {
+    ctx := &Context{URLs: map[string]string{"http://example.com/malware": "UNKNOWN", }}
 
+    str := addURLs(ctx)
+    assert.Equal(t, URLsTmpl, str, "should be equal")
 }
 
 func TestDoSendMail(t *testing.T) {
