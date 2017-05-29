e6dba118d39e702ca20c91853a91a0ae884dc032
Ollivier Robert <roberto@keltia.net>
2017-05-29 17:12:12 +0200

    Add test with fVerbose set to true. 100% coverage.

--- path_test.go
+++ path_test.go
@@ -23,6 +23,25 @@ func TestHandlePath(t *testing.T) {
 	assert.EqualValues(t, true, ctx.Paths[path2], "inserted")
 }
 
+func TestHandlePathVerbose(t *testing.T) {
+	ctx := &Context{
+		Paths: map[string]bool{},
+		URLs:  map[string]string{},
+	}
+	Plen := len(ctx.Paths)
+	fVerbose = true
+
+	path1 := "foo.exe"
+	handlePath(ctx, path1)
+	assert.EqualValues(t, Plen, len(ctx.Paths), "same value")
+
+	path2 := "foo.docx"
+	handlePath(ctx, path2)
+	assert.EqualValues(t, 1, len(ctx.Paths), "plus one")
+	assert.EqualValues(t, Plen+1, len(ctx.Paths), "plus one")
+	assert.EqualValues(t, true, ctx.Paths[path2], "inserted")
+}
+
 func TestEntryToPath(t *testing.T) {
 	str := "foo.doc|1a9ceab8d9b2358b46f2c767ccfc1317"
 
