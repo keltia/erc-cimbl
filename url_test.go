841af551337944efa25df173978ad0e79571208b
Ollivier Robert <roberto@keltia.net>
2017-05-30 16:07:51 +0200

    Add test for getProxy().

--- url_test.go
+++ url_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 	"net/http"
+	"os"
 )
 
 func TestSetupTransport(t *testing.T) {
@@ -16,6 +17,30 @@ func TestSetupTransport(t *testing.T) {
 	r, tr := setupTransport(ctx, url)
 	assert.Nil(t, r, "should be nil")
 	assert.Nil(t, tr, "should be nil")
+}
+
+func TestGetProxy(t *testing.T) {
+	// Cleanup
+	for _, env := range []string{
+		"http_proxy",
+		"https_proxy",
+		"HTTP_PROXY",
+		"HTTPS_PROXY",
+	} {
+		os.Unsetenv(env)
+	}
+	ctx := &Context{
+		URLs:   map[string]string{},
+	}
+
+	str := "http://pontonerywariva342.top/search.php"
+	req, transport := setupTransport(ctx, str)
+	assert.NotNil(t, req, "not nil")
+	assert.NotNil(t, transport, "not nil")
+
+	uri, err := getProxy(req)
+	assert.Nil(t, uri, "should be nil")
+	assert.NoError(t, err, "no error")
 }
 
 func TestDoCheck(t *testing.T) {
