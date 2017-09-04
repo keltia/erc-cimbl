cdc29e09f135ca33c5821b92908f544fb19a2229
Ollivier Robert <roberto@keltia.net>
2017-09-04 14:26:08 +0200

    Move "dbrc" category to the more logical "proxy".

--- config.go
+++ config.go
@@ -90,7 +90,7 @@ func loadDbrc(file string) (err error) {
 		flds := strings.Split(l, " ")
 
 		// Check what we need
-		if flds[0] == "cimbl" {
+		if flds[0] == "proxy" {
 			user = flds[1]
 			password = flds[2]
 			break
