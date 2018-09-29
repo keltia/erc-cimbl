66ae282c594b9db9d5b0949cc6ca83ef56263187
Ollivier Robert <roberto@keltia.net>
2018-09-30 00:21:03 +0200

    Add tests for setup().

--- main_test.go
+++ main_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
+	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCheckFilename(t *testing.T) {
@@ -13,4 +16,42 @@ func TestCheckFilename(t *testing.T) {
 	file = "CIMBL-0666-CERTS.csv"
 	res = checkFilename(file)
 	assert.True(t, res, "should be true")
+}
+
+func TestSetup(t *testing.T) {
+	baseDir = "test"
+
+	ctx := setup()
+	assert.NotNil(t, ctx)
+
+	assert.NotNil(t, ctx.config)
+	assert.Empty(t, ctx.URLs)
+	assert.Empty(t, ctx.Paths)
+	assert.Nil(t, ctx.tempdir)
+}
+
+func TestSetupNone(t *testing.T) {
+	baseDir = "testx"
+
+	ctx := setup()
+	assert.NotNil(t, ctx)
+
+	assert.Empty(t, ctx.config)
+	assert.Empty(t, ctx.URLs)
+	assert.Empty(t, ctx.Paths)
+	assert.Nil(t, ctx.tempdir)
+}
+
+func TestSetupProxy(t *testing.T) {
+	baseDir = "test"
+	os.Setenv("NETRC", filepath.Join(".", "test", "test-netrc"))
+
+	ctx := setup()
+	assert.NotNil(t, ctx)
+
+	assert.NotNil(t, ctx.config)
+	assert.Empty(t, ctx.URLs)
+	assert.Empty(t, ctx.Paths)
+	assert.Nil(t, ctx.tempdir)
+	assert.NotNil(t, ctx.proxyauth)
 }
