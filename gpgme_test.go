84982699210f4e27f43f769a75c04c0e3daae4f8
Ollivier Robert <roberto@keltia.net>
2018-10-03 19:03:18 +0200

    Add test for error-ing Decrypt().

--- gpgme_test.go
+++ gpgme_test.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/keltia/sandbox"
+	"github.com/proglottis/gpgme"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+type NullGPGError struct{}
+
+var ErrFakeGPGError = fmt.Errorf("fake error")
+
+func (NullGPGError) Decrypt(r io.Reader) (*gpgme.Data, error) {
+	return &gpgme.Data{}, ErrFakeGPGError
+}
 
 func TestNullGPG_Decrypt(t *testing.T) {
 	baseDir = "testdata"
@@ -37,6 +47,36 @@ func TestNullGPG_Decrypt(t *testing.T) {
 
 	plain, err := ctx.gpg.Decrypt(fh)
 	assert.NoError(t, err)
+	assert.Empty(t, plain)
+}
+
+func TestNullGPGError_Decrypt(t *testing.T) {
+	baseDir = "testdata"
+	config, err := loadConfig()
+	assert.NoError(t, err)
+
+	fVerbose = true
+
+	snd, err := sandbox.New("test")
+	require.NoError(t, err)
+	defer snd.Cleanup()
+
+	ctx := &Context{
+		config:  config,
+		Paths:   map[string]bool{},
+		URLs:    map[string]string{},
+		tempdir: snd,
+		gpg:     NullGPGError{},
+	}
+
+	file := "testdata/CIMBL-0666-CERTS.zip.asc"
+	fh, err := os.Open(file)
+	require.NoError(t, err)
+	require.NotNil(t, fh)
+
+	plain, err := ctx.gpg.Decrypt(fh)
+	assert.Error(t, err)
+	assert.Equal(t, ErrFakeGPGError, err)
 	assert.Empty(t, plain)
 }
 
