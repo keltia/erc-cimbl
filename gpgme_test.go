72e20d26b2748f94ecddf05800bdb399fbb1d5c8
Ollivier Robert <roberto@keltia.net>
2018-10-03 20:27:01 +0200

    Test decryptFile() with NullGPGError.

--- gpgme_test.go
+++ gpgme_test.go
@@ -122,6 +122,48 @@ func TestDecryptFileNull(t *testing.T) {
 	assert.Empty(t, plain)
 }
 
+func TestDecryptFileNullError(t *testing.T) {
+	baseDir = "testdata"
+	config, err := loadConfig()
+	assert.NoError(t, err)
+
+	fVerbose = true
+
+	snd, err := sandbox.New("test")
+	require.NoError(t, err)
+	defer snd.Cleanup()
+
+	ctx := &Context{
+		config:  config,
+		Paths:   map[string]bool{},
+		URLs:    map[string]string{},
+		tempdir: snd,
+		gpg:     NullGPGError{},
+	}
+
+	file, err := filepath.Abs("testdata/CIMBL-0666-CERTS.zip.asc")
+	require.NoError(t, err)
+
+	fh, err := os.Open(file)
+	require.NoError(t, err)
+	require.NotNil(t, fh)
+
+	// create our fake zip file
+	dfh, err := os.Create(filepath.Join(snd.Cwd(), "CIMBL-0666-CERTS.zip"))
+	require.NoError(t, err)
+	defer dfh.Close()
+
+	// copy into sandbox
+	_, err = io.Copy(dfh, fh)
+	require.NoError(t, err)
+
+	snd.Enter()
+	plain, err := decryptFile(ctx, file)
+	snd.Exit()
+	assert.Error(t, err)
+	assert.Empty(t, plain)
+}
+
 func TestDecryptFileNone(t *testing.T) {
 	baseDir = "testdata"
 	config, err := loadConfig()
