522fb299a717d20e43f3d05424a2c6ff30c93bca
Ollivier Robert <roberto@keltia.net>
2019-02-21 18:05:31 +0100

    Add REfile to struct Config.  Be consistent in return. Fix tests to match.

--- config.go
+++ config.go
@@ -17,15 +17,17 @@ type Config struct {
 	Subject string
 	Cc      string
 	KeyID   string
+
+	// RE to check filenames
+	REFile string
 }
 
-func loadConfig() (c *Config, err error) {
+func loadConfig() (*Config, error) {
 	file := filepath.Join(baseDir, configName)
 
 	// Check if there is any config file
-	if _, err = os.Stat(file); err != nil {
-		c = &Config{}
-		return
+	if _, err := os.Stat(file); err != nil {
+		return &Config{}, err
 	}
 
 	verbose("file=%s, found it", file)
@@ -33,14 +35,19 @@ func loadConfig() (c *Config, err error) {
 	// Read it
 	buf, err := ioutil.ReadFile(file)
 	if err != nil {
-		return c, fmt.Errorf("Can not read %s", file)
+		return &Config{}, fmt.Errorf("Can not read %s", file)
 	}
 
-	cnf := Config{}
+	var cnf Config
+
 	err = toml.Unmarshal(buf, &cnf)
 	if err != nil {
-		return c, fmt.Errorf("Error parsing toml %s: %v", file, err)
+		return &Config{}, fmt.Errorf("Error parsing toml %s: %v", file, err)
 	}
-	c = &cnf
-	return
+
+	// Ensure we got sensible defaults
+	if cnf.REFile == "" {
+		cnf.REFile = `(?i:CIMBL-\d+-(CERTS|EU)\.(csv|zip)(\.asc|))`
+	}
+	return &cnf, nil
 }
