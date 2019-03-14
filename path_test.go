3f908916f5107b7b8817ed520ccde4a9582e4a12
Ollivier Robert <roberto@keltia.net>
2019-03-14 16:36:06 +0100

    Handle fNoPaths.

--- path_test.go
+++ path_test.go
@@ -20,6 +20,24 @@ func TestHandlePath(t *testing.T) {
 	assert.NoError(t, err)
 }
 
+func TestHandlePathno(t *testing.T) {
+	ctx := &Context{}
+
+	fNoPaths = true
+
+	path1 := "foo.exe"
+	r, err := handlePath(ctx, path1)
+	assert.Empty(t, r)
+	assert.NoError(t, err)
+
+	path2 := "foo.docx"
+	r, err = handlePath(ctx, path2)
+	assert.Empty(t, r)
+	assert.NoError(t, err)
+
+	fNoPaths = false
+}
+
 func TestHandlePathVerbose(t *testing.T) {
 	ctx := &Context{}
 	fVerbose = true
