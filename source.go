395a6034b84aa0f798eb7b7daab20c508e9de6c5
Ollivier Robert <roberto@keltia.net>
2019-07-25 17:08:37 +0200

    Hack to still get a meaningful file list.

--- source.go
+++ source.go
@@ -124,6 +124,8 @@ func (l *List) AddFromFile(fn string) (*List, error) {
 		return l, errors.Wrap(err, "single/readfile")
 	}
 
+	// Update the file list.
+	l.ctx.files = append(l.ctx.files, filepath.Base(base))
 	return l.ReadFromCSV(buf)
 }
 
