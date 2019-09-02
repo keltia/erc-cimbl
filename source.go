11d5e855a91167e2ba8604102cfdc3f511e1f63b
Ollivier Robert <roberto@keltia.net>
2019-09-02 14:08:30 +0200

    Add #Length().

--- source.go
+++ source.go
@@ -81,7 +81,7 @@ func NewList(files []string) *List {
 
 			l, err = l.AddFromFile(e)
 			if err != nil {
-				log.Printf("%v: reading error", e)
+				log.Printf("%v: reading error: %v", e, errors.Wrap(err, "AddFromFile"))
 			}
 		} else {
 			log.Printf("invalid filename")
@@ -94,6 +94,10 @@ func (l *List) Add(s Sourcer) *List {
 	debug("adding %#v", s)
 	l.s = append(l.s, s)
 	return l
+}
+
+func (l *List) Length() int {
+	return len(l.s)
 }
 
 func (l *List) AddFromFile(fn string) (*List, error) {
@@ -265,7 +269,6 @@ func (l *List) Check(ctx *Context) *Results {
 	}
 
 	close(queue)
-	close(ins)
 
 	wg.Wait()
 	r.files = l.Files()
